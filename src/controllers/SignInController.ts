d3f9e1eff8a77d3c9f5b65cdb25351d23c8b5073
Gabriel <reistomm@gmail.com>
2025-07-19 13:38:50 -0300

    feat: add Zod validation to SignIn and SignUp controllers and update package dependencies

--- src/controllers/SignInController.ts
+++ src/controllers/SignInController.ts
@@ -1,10 +1,23 @@
-import { access } from "fs";
 import { HttpResponse, HtttpRequest } from "../types/Http";
-import { ok } from "../utils/http";
+import { badRequest, created, ok } from "../utils/http";
+import { z } from 'zod';
+
+const schema = z.object({
+    email: z.email(),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+});
 
 export class SignInController {
-    static async handle(request: HtttpRequest): Promise<HttpResponse> {
-        return ok({ accessToken: "Token de acesso gerado com sucesso!" })
+    static async handle({body}: HtttpRequest): Promise<HttpResponse> {
+        const { success, error, data } = schema.safeParse(body);
 
+        if (!success) {
+            return badRequest({errors: error.issues})
+        }
+
+
+        return created({
+            data,
+        });
     }
 }
--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -1,11 +1,33 @@
-import { access } from "fs";
 import { HttpResponse, HtttpRequest } from "../types/Http";
-import { created } from "../utils/http";
+import { badRequest, created } from "../utils/http";
+import z from "zod";
+
+const schema = z.object({
+    goal: z.enum(['lose', 'maintain', 'gain']),
+    gender: z.enum(['male', 'female']),
+    birthDate: z.iso.date(),
+    height: z.number(),
+    weight: z.number(),
+    activityLevel: z.number().int().min(1).max(5),
+    account: z.object({
+        email: z.email(),
+        password: z.string().min(6),
+        name: z.string().min(1),
+    }),
+});
 
 export class SignUpController {
-    static async handle(request: HtttpRequest): Promise<HttpResponse> {
+    static async handle({body}: HtttpRequest): Promise<HttpResponse> {
+
+        const { success, error, data } = schema.safeParse(body);
+
+        if (!success) {
+            return badRequest({errors: error.issues})
+        }
+
+
         return created({
-            accessToken: "Token de acesso gerado com sucesso!"
+            data,
         });
     }
 }
