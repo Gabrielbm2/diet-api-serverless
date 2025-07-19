3cf8b60b1b25950cf8e8b00148a910186deae2be
Gabriel <reistomm@gmail.com>
2025-07-19 18:41:48 -0300

    feat: create meals table with input types and statuses, and update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, uuid, varchar, date } from "drizzle-orm/pg-core";
+import { integer, pgTable, uuid, varchar, date, json, pgEnum, timestamp } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -15,4 +15,22 @@ export const usersTable = pgTable("users", {
     proteins: integer().notNull(),
     carboHydrates: integer().notNull(),
     fats: integer().notNull(),
+});
+
+export const mealStatus = pgEnum('meal_status', ['uploading', 'queued', 'processing', 'success', 'failed']);
+
+export const mealInputType = pgEnum('meal_input_type', ['audio', 'picture']);
+
+export const mealsTable = pgTable('meals', {
+  id: uuid().primaryKey().defaultRandom(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  status: mealStatus().notNull(),
+  inputType: mealInputType('input_type').notNull(),
+  inputFileKey: varchar('input_file_key', { length: 255 }).notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  icon: varchar({ length: 100 }).notNull(),
+  foods: json(),  
+  createdAt: timestamp('created_at').notNull().defaultNow(),
 });
