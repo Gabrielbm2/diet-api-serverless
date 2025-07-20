e4a5aa9fe337a6ab7bc5afc04b7ad03214cf51af
Gabriel <reistomm@gmail.com>
2025-07-20 08:42:07 -0300

    feat: implement CreateMealController and associated handler for meal creation with S3 upload

---
+++ src/controllers/CreateMealController.ts
@@ -0,0 +1,46 @@
+import { HttpResponse, ProtectedHttpRequest } from "../types/Http";
+import { badRequest, created, ok, unauthorized } from "../utils/http";
+import { randomUUID } from 'crypto';
+import { s3Client } from '../clients/s3Client';
+
+import { mealsTable} from "../db/schema";
+import { db } from "../db";
+import z from "zod";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const schema = z.object({
+    fileType: z.enum(['audio/m4a', 'image/jpeg'])
+})
+
+export class CreateMealController {
+  static async handle({ userId, body }: ProtectedHttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(body);
+
+    if (!success) {
+      return badRequest({ errors: error.issues });
+    }
+
+    const fileId = randomUUID();
+    const ext = data.fileType === 'audio/m4a' ? '.m4a' : '.jpg';
+    const fileKey = `${fileId}${ext}`;
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.BUCKET_NAME,
+      Key: fileKey,
+    });
+
+    const presignedURL = await getSignedUrl(s3Client, command, { expiresIn: 600 });
+
+    const [meal] = await db.insert(mealsTable).values({
+        userId,
+        icon: '',
+        inputFileKey: 'input_file_key',
+        inputType: data.fileType === 'audio/m4a' ? 'audio' : 'picture',
+        name: '',
+        status: 'uploading',
+        foods: [],
+    }).returning({ id: mealsTable.id });
+    return created({ mealId: meal.id, uploadURL: presignedURL, });
+  }
+}
--- src/functions/createMeal.ts
+++ src/functions/createMeal.ts
@@ -1,15 +1,16 @@
 import {APIGatewayProxyEventV2} from 'aws-lambda'
 import { parseResponse } from "../utils/parseResponse";
-import { MeController } from '../controllers/MeController';
+import { CreateMealController } from '../controllers/CreateMealController';
 import { parseProtectedEvent } from '../utils/parseProtectedEvent';
 import { unauthorized } from '../utils/http';
 
 export async function handler(event: APIGatewayProxyEventV2) {
     try {
         const request = parseProtectedEvent(event);
-        const response = await MeController.handle(request);
+        const response = await CreateMealController.handle(request);
+        
         return parseResponse(response);
-    } catch {
+    } catch  {
         return parseResponse(
             unauthorized({ 
                 error: "Unauthorized: Invalid access token" 
